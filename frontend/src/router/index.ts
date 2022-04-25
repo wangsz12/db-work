12b3026dbc53e9002561e56813736076abe9bd43
wangsz12 <1002590897@qq.com>
2022-04-26 00:18:44 +0800

    feature: 增加鉴权 无token跳转登录

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -6,4 +6,13 @@ const router = createRouter({
   history: createWebHistory()
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'login' && !localStorage.getItem('token')) {
+    next('/login')
+  }
+  else {
+    next()
+  }
+})
+
 export default router

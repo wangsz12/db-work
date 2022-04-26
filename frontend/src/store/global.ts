cbd77edd6f758e62c0f828bb5ad57e94f8630e36
wangsz12 <1002590897@qq.com>
2022-04-26 17:14:18 +0800

    feature: 全局变量的pinia store

---
+++ frontend/src/store/global.ts
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia"
+
+export const useGlobalStore = defineStore({
+  id: 'global',
+  state: () => {
+    return {
+      selectedItemName: 'Index'
+    }
+  },
+  actions: {
+    setSelectedItemName(selectedItemName: string) {
+      this.selectedItemName = selectedItemName
+    }
+  }
+})

a10e94989a6f0c86a31cf318a48388a08a299c94
wangsz12 <1002590897@qq.com>
2022-04-26 17:13:15 +0800

    feature: 将新页面添加至router map

--- frontend/src/router/router-map.ts
+++ frontend/src/router/router-map.ts
@@ -3,13 +3,42 @@ import { RouteRecordRaw } from "vue-router"
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    name: 'index',
-    component: () => import('@/layout/main-layout.vue')
+    name: 'layout',
+    component: () => import('@/layout/main-layout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'index',
+        component: () => import('@/pages/index.vue')
+      }
+    ]
+  },
+  {
+    path: '/books',
+    name: 'books',
+    component: () => import('@/layout/main-layout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'all-books',
+        component: () => import('@/pages/books/all-books.vue')
+      },
+      {
+        path: 'lend',
+        name: 'lend-books',
+        component: () => import('@/pages/books/lend.vue')
+      }
+    ]
   },
   {
     path: '/login',
     name: 'login',
     component: () => import('@/pages/login.vue')
+  },
+  {
+    path: '/:pathMatch(.*)',
+    name: 'not-found',
+    component: () => import('@/pages/404.vue')
   }
 ]
 

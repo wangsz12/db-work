31c380fd719143ebc943937773f1919a35620d4e
wangsz12 <1002590897@qq.com>
2022-04-26 17:12:56 +0800

    feature: 加入message提示 feature: 集成nprogress进度条

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,8 @@
+import { useMessage } from "@/utils"
 import { createRouter, createWebHistory } from "vue-router"
 import routes from './router-map'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const router = createRouter({
   routes,
@@ -7,12 +10,19 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
+  NProgress.start()
+
   if (to.name !== 'login' && !localStorage.getItem('token')) {
+    useMessage().warning('请先登录')
     next('/login')
   }
   else {
     next()
   }
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router

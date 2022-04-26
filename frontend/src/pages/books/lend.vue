f0d486f3d201ddc3434fb54cd040ce53cb5834a5
wangsz12 <1002590897@qq.com>
2022-04-26 17:12:38 +0800

    init: 初始化借出登记页面

---
+++ frontend/src/pages/books/lend.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="form-box" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .container {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .form-box {
+      @extend .component;
+
+      width: 42rem;
+      padding: 2rem {
+        bottom: 3rem;
+      };
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+
+      .form {
+        width: 25rem;
+        margin-top: .8rem;
+
+        .submit-box {
+          display: flex;
+          justify-content: center;
+
+          button {
+            width: 30%;
+            height: 2.5rem;
+            font-size: 14px;
+            margin-top: .8rem;
+          }
+        }
+      }
+    }
+  }
+</style>

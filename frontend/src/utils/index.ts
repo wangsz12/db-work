038b7099ea919168396a50aa676050c9eb7b7bc1
wangsz12 <1002590897@qq.com>
2022-04-26 17:15:43 +0800

    feature: 增加arco表格每列居中处理函数

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,6 +1,13 @@
-import { Message } from '@arco-design/web-vue'
+import { Message, TableColumnData } from '@arco-design/web-vue'
 import '@arco-design/web-vue/es/message/style/css.js'
 
 export function useMessage() {
   return Message
+}
+
+export function withAlignCenter(columns: TableColumnData[]): TableColumnData[] {
+  return columns.map((item) => Object.defineProperty(item, 'align', {
+    value: 'center',
+    enumerable: true
+  }))
 }

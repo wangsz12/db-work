9e3afe3ece39038909e7cbfa292d51983db45426
wangsz12 <1002590897@qq.com>
2022-04-28 14:05:42 +0800

    feature: 完成 获取罚款记录、按借阅证号获取记录、按编号获取记录、缴纳罚款 的模拟接口

---
+++ frontend/src/apis/fines.ts
@@ -0,0 +1,48 @@
+import { mockAPIReturn } from "@/utils"
+import { PayFineRequest } from "@/utils/types"
+
+export function getFines(page = 1) {
+  console.log('page: ', page)
+  return mockAPIReturn({
+    total: 67,
+    fines: Array(10).fill('').map((_, index) => ({
+      id: `f${('00000' + (10 * page + index + 1)).slice(-5)}`,
+      cardID: '1000001',
+      bookID: 'b0000001',
+      name: '图解HTTP',
+      author: '[日]上野 宣',
+      amount: 3.27,
+      paid: false
+    }))
+  })
+}
+
+export function getFineRecordsByCardID(cardID: string) {
+  console.log('cardID: ', cardID)
+  return mockAPIReturn([
+    {
+      id: 'f00001',
+      record: '图解HTTP / [日]上野 宣 / ￥3.27'
+    }
+  ])
+}
+
+export function getFineRecordsByID(id: string) {
+  console.log('id: ', id)
+  return mockAPIReturn({
+    id: 'f00001',
+    record: '图解HTTP / [日]上野 宣 / ￥3.27',
+    book: {
+      id: 'b000001',
+      name: '图解HTTP',
+      author: '[日]上野 宣',
+      isbn: '9798115351531'
+    },
+    fine: 3.27
+  })
+}
+
+export function payFine({cardID, fineID}: PayFineRequest) {
+  console.log('cardID, fineID: ', cardID, fineID)
+  return mockAPIReturn()
+}

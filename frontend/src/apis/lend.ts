b4a6cb8883c4600661066a1e323f2973bf15b8f0
wangsz12 <1002590897@qq.com>
2022-04-29 00:05:02 +0800

    feature: 新增 按页码查询借出记录 的模拟接口

--- frontend/src/apis/lend.ts
+++ frontend/src/apis/lend.ts
@@ -19,10 +19,10 @@ export function getLendRecordsByCardID(cardID: string) {
 export function getLendRecordsByID(id: string) {
   console.log('id: ', id)
   return mockAPIReturn({
-    id: 'b000001',
+    id: 'L000001',
     record: '图解HTTP / [日]上野 宣 / 2022-04-26',
     book: {
-      id: 'b000001',
+      id: 'B000001',
       name: '图解HTTP',
       author: '[日]上野 宣',
       isbn: '9798115351531'
@@ -31,5 +31,23 @@ export function getLendRecordsByID(id: string) {
     duration: 2,
     isOverdue: true,
     fine: 30.26
+  })
+}
+
+export function getLendRecordsByPage(page = 1) {
+  console.log('page: ', page)
+  return mockAPIReturn({
+    total: 476,
+    records: Array(10).fill('').map((_, index) => ({
+      id: `L${('0000000' + (10 * page + index + 1)).slice(-7)}`,
+      book: {
+        id: 'b000001',
+        name: '图解HTTP',
+        author: '[日]上野 宣'
+      },
+      cardID: '1000001',
+      date: '2022-04-26',
+      duration: 2
+    }))
   })
 }

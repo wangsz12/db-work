d0327a51f67ea7af712f1ed80cd5b8c81e37957d
wangsz12 <1002590897@qq.com>
2022-05-05 15:10:33 +0800

    feature: 登录模块

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthDao } from './auth.dao';
+import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
+  providers: [AuthService, AuthDao],
 })
 export class AuthModule {}

08b50bc7ef42520e981463f69c5fe10dcbb51980
wangsz12 <1002590897@qq.com>
2022-05-05 20:24:25 +0800

    feature: 读者证模块

--- backend/src/cards/cards.module.ts
+++ backend/src/cards/cards.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CardsController } from './cards.controller';
+import { CardsDao } from './cards.dao';
+import { CardsService } from './cards.service';
 
 @Module({
   controllers: [CardsController],
+  providers: [CardsService, CardsDao],
 })
 export class CardsModule {}

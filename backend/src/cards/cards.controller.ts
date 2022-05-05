083cbd222757d63214f52ef4478f2bcce2080269
wangsz12 <1002590897@qq.com>
2022-05-05 20:09:55 +0800

    feature: 读者证模块的controller

--- backend/src/cards/cards.controller.ts
+++ backend/src/cards/cards.controller.ts
@@ -1,38 +1,33 @@
 import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { ResponseData } from 'src/types';
-import { trueReturn } from 'src/utils';
+import { falseReturn, trueReturn } from 'src/utils';
+import { CardsService } from './cards.service';
 import { CreateCardDto } from './dto/create-card.dto';
 
 @Controller('cards')
 export class CardsController {
+  constructor(private readonly cardsService: CardsService) {}
+
   @Get()
-  findAll(@Query() page = 1): ResponseData {
+  async findAll(@Query('page') page = 1): Promise<ResponseData> {
     return trueReturn({
-      total: 1536,
-      cards: Array(10)
-        .fill('')
-        .map((_, index) => ({
-          id: `1${('0000000' + (10 * page + index + 1)).slice(-7)}`,
-          name: '李华',
-          gender: '男',
-          contact: '13012345678',
-          address: 'XX市XX区XX路XX小区',
-          haveLent: 12,
-        })),
+      total: await this.cardsService.findTotalQuantity(),
+      cards: await this.cardsService.findAll(page),
     });
   }
 
   @Post()
-  create(
+  async create(
     @Body() { name, gender, contact, address }: CreateCardDto,
-  ): ResponseData {
+  ): Promise<ResponseData> {
     return trueReturn({
-      id: 'R000001',
+      id: await this.cardsService.create(name, gender, contact, address),
     });
   }
 
   @Delete()
-  delete(@Query('id') id: string) {
-    return trueReturn();
+  async delete(@Query('id') id: string): Promise<ResponseData> {
+    const res = await this.cardsService.delete(id);
+    return res ? trueReturn() : falseReturn();
   }
 }

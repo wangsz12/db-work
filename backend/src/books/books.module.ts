32175e08c3f5fabc988911da5e7d3c79cde666c9
wangsz12 <1002590897@qq.com>
2022-05-05 15:04:58 +0800

    feature: 图书模块

--- backend/src/books/books.module.ts
+++ backend/src/books/books.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BooksController } from './books.controller';
+import { BooksDao } from './books.dao';
+import { BooksService } from './books.service';
 
 @Module({
   controllers: [BooksController],
+  providers: [BooksService, BooksDao],
 })
 export class BooksModule {}

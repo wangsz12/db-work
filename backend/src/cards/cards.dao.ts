c1453597ee4f3c1f17ebbd2ce3fabcb8741b3f3c
wangsz12 <1002590897@qq.com>
2022-05-05 20:10:04 +0800

    feature: 读者证模块的Dao层

---
+++ backend/src/cards/cards.dao.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common';
+import { executeSQL } from 'src/utils/mysql';
+import { ReaderCardEntity } from './entity/reader-card.entity';
+
+function DB2ReaderCardEntity(result: any[]): ReaderCardEntity[] {
+  return result.map(
+    ({ id, name, gender, contact, address }) =>
+      new ReaderCardEntity(id, name, gender, contact, address),
+  );
+}
+
+@Injectable()
+export class CardsDao {
+  async findAll(page: number): Promise<ReaderCardEntity[]> {
+    const res = await executeSQL('SELECT * FROM reader LIMIT ?,10', [
+      10 * (page - 1),
+    ]);
+
+    return DB2ReaderCardEntity(res);
+  }
+
+  async findTotalQuantity(): Promise<number> {
+    const res = await executeSQL('SELECT COUNT(*) AS quantity FROM reader');
+
+    return res[0].quantity;
+  }
+
+  async findNextID(): Promise<string> {
+    const last = await executeSQL(
+      'SELECT card_id FROM reader ORDER BY card_id DESC LIMIT 1',
+    );
+
+    return `R${('000000' + (parseInt(last[0].card_id.slice(1)) + 1)).slice(
+      -7,
+    )}`;
+  }
+
+  async create(card: ReaderCardEntity): Promise<boolean> {
+    try {
+      await executeSQL('INSERT INTO reader VALUES (?,?,?,?,?)', [
+        card.id,
+        card.name,
+        card.gender,
+        card.contact,
+        card.address,
+      ]);
+      return true;
+    } catch (err) {
+      console.log(err.sqlMessage);
+      return false;
+    }
+  }
+
+  async delete(id: string): Promise<boolean> {
+    try {
+      await executeSQL('DELETE FROM reader WHERE card_id=?', [id]);
+      return true;
+    } catch (err) {
+      console.log(err.sqlMessage);
+      return false;
+    }
+  }
+}

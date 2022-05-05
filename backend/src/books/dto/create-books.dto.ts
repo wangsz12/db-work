d1341344c97837d9e6198c26e0fd5beb94dbafb9
wangsz12 <1002590897@qq.com>
2022-05-05 15:05:25 +0800

    feature: 新增图书的dto

--- backend/src/books/dto/create-books.dto.ts
+++ backend/src/books/dto/create-books.dto.ts
@@ -1,10 +1,26 @@
-import { IsInt, IsString, Min } from 'class-validator';
+import { IsInt, IsNumber, IsString, Min } from 'class-validator';
 
 export class CreateBooksDto {
   @IsString()
-  readonly bookID: string;
+  readonly publisherID: string;
+
+  @IsString()
+  readonly name: string;
+
+  @IsString()
+  readonly author: string;
 
   @IsInt()
   @Min(0)
   readonly quantity: number;
+
+  @IsString()
+  readonly category: string;
+
+  @IsString()
+  readonly isbn: string;
+
+  @IsNumber()
+  @Min(0)
+  readonly price: number;
 }

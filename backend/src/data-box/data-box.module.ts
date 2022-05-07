3f077218d94deff507bf6b4084e8064be03dcaed
wangsz12 <1002590897@qq.com>
2022-05-07 21:36:41 +0800

    feature: 自动注入service和Dao

--- backend/src/data-box/data-box.module.ts
+++ backend/src/data-box/data-box.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DataBoxController } from './data-box.controller';
+import { DataBoxDao } from './data-box.dao';
+import { DataBoxService } from './data-box.service';
 
 @Module({
   controllers: [DataBoxController],
+  providers: [DataBoxService, DataBoxDao],
 })
 export class DataBoxModule {}

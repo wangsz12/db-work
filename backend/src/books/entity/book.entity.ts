75bbc28b5868a55c47d4eba28765677f9a68691f
wangsz12 <1002590897@qq.com>
2022-05-10 00:02:09 +0800

    feature: 加入类型判断

--- backend/src/books/entity/book.entity.ts
+++ backend/src/books/entity/book.entity.ts
@@ -1,3 +1,5 @@
+import { IsInt, IsNumber, IsString, Length, Min } from 'class-validator';
+
 export class BookEntity {
   constructor(
     id: string,
@@ -19,19 +21,29 @@ export class BookEntity {
     this.price = price;
   }
 
+  @IsString()
   id: string;
 
+  @IsString()
   publisherID?: string;
 
+  @IsString()
   name?: string;
 
+  @IsString()
   author?: string;
 
+  @IsInt()
+  @Min(0)
   quantity?: number;
 
+  @IsString()
   category?: string;
 
+  @IsString()
+  @Length(13)
   isbn?: string;
 
+  @IsNumber()
   price?: number;
 }

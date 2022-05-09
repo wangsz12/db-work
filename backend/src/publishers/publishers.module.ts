12f2af60a1d5d2e2e30bd38cc3e4ea8ba747cfcb
wangsz12 <1002590897@qq.com>
2022-05-10 00:03:08 +0800

    feature: 出版社模块

--- backend/src/publishers/publishers.module.ts
+++ backend/src/publishers/publishers.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PublishersController } from './publishers.controller';
+import { PublishersDao } from './publishers.dao';
+import { PublishersService } from './publishers.service';
 
 @Module({
   controllers: [PublishersController],
+  providers: [PublishersService, PublishersDao],
 })
 export class PublishersModule {}

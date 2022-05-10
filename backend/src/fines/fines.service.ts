8501325ce7ca1508ff4ee5226c756368e6c3fb58
wangsz12 <1002590897@qq.com>
2022-05-10 14:02:19 +0800

    feature: 罚款模块的service

---
+++ backend/src/fines/fines.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { FindAllDto } from './dto/find-all.dto';
+import { FineEntity } from './entity/fine.entity';
+import { FinesDao } from './fines.dao';
+
+@Injectable()
+export class FinesService {
+  constructor(private readonly finesDao: FinesDao) {}
+
+  async findAll(page: number): Promise<FindAllDto> {
+    return {
+      total: await this.finesDao.findTotalQuantity(),
+      fines: await this.finesDao.findAll(page),
+    };
+  }
+
+  async findOne(id: string): Promise<FineEntity> {
+    return await this.finesDao.findOneByID(id);
+  }
+
+  async findAllByCard(id: string): Promise<FineEntity[]> {
+    return await this.finesDao.findAllByCard(id);
+  }
+
+  async create(
+    adminID: string,
+    lendID: string,
+    amount: number,
+  ): Promise<boolean> {
+    if (await this.finesDao.checkExisted(lendID)) {
+      throw new Error('该条记录已有罚款');
+    }
+
+    const fine = new FineEntity(
+      await this.finesDao.findNextID(),
+      adminID,
+      new FineEntity(lendID),
+      amount,
+      false,
+    );
+
+    return await this.finesDao.create(fine);
+  }
+
+  async payFine(id: string): Promise<boolean> {
+    const fine = await this.finesDao.findOneByID(id);
+    if (fine.isPaid) {
+      throw new Error('罚款已缴纳');
+    }
+
+    return await this.finesDao.update(id);
+  }
+}

932745fce272bc2b631f94b2adf65c9db762f2ec
wangsz12 <1002590897@qq.com>
2022-05-10 16:16:27 +0800

    feature: 自动注入借出和归还的service与dao

--- backend/src/records/records.module.ts
+++ backend/src/records/records.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LendController } from './lend/lend.controller';
 import { ReturnController } from './return/return.controller';
+import { LendService } from './lend/lend.service';
+import { LendDao } from './lend/lend.dao';
+import { ReturnService } from './return/return.service';
+import { ReturnDao } from './return/return.dao';
 
 @Module({
   controllers: [LendController, ReturnController],
+  providers: [LendService, LendDao, ReturnService, ReturnDao],
 })
 export class RecordsModule {}

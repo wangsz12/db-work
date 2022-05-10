21b5b051a5c458db7117b0c2d456cdbd0217545e
wangsz12 <1002590897@qq.com>
2022-05-11 00:10:41 +0800

    feature: 添加需要自动注入的罚款模块service与dao

--- backend/src/records/records.module.ts
+++ backend/src/records/records.module.ts
@@ -5,9 +5,18 @@ import { LendService } from './lend/lend.service';
 import { LendDao } from './lend/lend.dao';
 import { ReturnService } from './return/return.service';
 import { ReturnDao } from './return/return.dao';
+import { FinesService } from 'src/fines/fines.service';
+import { FinesDao } from 'src/fines/fines.dao';
 
 @Module({
   controllers: [LendController, ReturnController],
-  providers: [LendService, LendDao, ReturnService, ReturnDao],
+  providers: [
+    LendService,
+    LendDao,
+    ReturnService,
+    ReturnDao,
+    FinesService,
+    FinesDao,
+  ],
 })
 export class RecordsModule {}

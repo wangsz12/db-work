5a297a91abdfe05836704a132d95dc227518f9c4
wangsz12 <1002590897@qq.com>
2022-05-10 14:03:21 +0800

    feature: 加入长度限制

--- backend/src/fines/dto/update-fine.dto.ts
+++ backend/src/fines/dto/update-fine.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from 'class-validator';
+import { IsString, Length } from 'class-validator';
 
 export class UpdateFineDto {
   @IsString()
+  @Length(6)
   readonly id: string;
 }

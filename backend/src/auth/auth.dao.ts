209715d802093378deb4ef4ed3d5987073483dc1
wangsz12 <1002590897@qq.com>
2022-05-11 00:09:12 +0800

    feature: 增加登录时获取管理员ID

--- backend/src/auth/auth.dao.ts
+++ backend/src/auth/auth.dao.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { executeSQL } from 'src/utils/mysql';
+import { AdminEntity } from './entity/admin.entity';
+
+function DB2AdminEntity(result: any[]): AdminEntity[] {
+  return result.map(({ id, name }) => new AdminEntity(id, name));
+}
 
 @Injectable()
 export class AuthDao {
-  async login(account: string, password: string): Promise<boolean> {
+  async login(
+    account: string,
+    password: string,
+  ): Promise<AdminEntity | boolean> {
     const res = await executeSQL(
-      'SELECT name FROM admin WHERE account=? AND password=?',
+      'SELECT id,name FROM admin WHERE account=? AND password=?',
       [account, password],
     );
 
-    return res.length ? res[0].name : null;
+    return res.length ? DB2AdminEntity(res)[0] : false;
   }
 }

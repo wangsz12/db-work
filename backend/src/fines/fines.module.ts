541655261453ef5c6f0dbb3fac438e805cdc3ff1
wangsz12 <1002590897@qq.com>
2022-05-10 14:02:09 +0800

    feature: 引入自动注入的service与dao

--- backend/src/fines/fines.module.ts
+++ backend/src/fines/fines.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FinesController } from './fines.controller';
+import { FinesDao } from './fines.dao';
+import { FinesService } from './fines.service';
 
 @Module({
   controllers: [FinesController],
+  providers: [FinesService, FinesDao],
 })
 export class FinesModule {}

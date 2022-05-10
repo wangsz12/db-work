c89f3942574acad3ffad27ff80198430ca461dfb
wangsz12 <1002590897@qq.com>
2022-05-10 16:16:51 +0800

    feature: 借出模块controller

--- backend/src/records/lend/lend.controller.ts
+++ backend/src/records/lend/lend.controller.ts
@@ -1,64 +1,60 @@
 import { Body, Controller, Get, Param, Post, Query, Res } from '@nestjs/common';
 import { ResponseData } from 'src/types';
-import { trueReturn } from 'src/utils';
+import { falseReturn, trueReturn } from 'src/utils';
 import { CreateLendRecordDto } from './dto/create-lend-record.dto';
+import { LendService } from './lend.service';
 
 @Controller('records/lend')
 export class LendController {
+  constructor(private readonly lendService: LendService) {}
+
   @Get()
-  findAll(@Query('page') page = 1): ResponseData {
-    return trueReturn({
-      total: 476,
-      records: Array(10)
-        .fill('')
-        .map((_, index) => ({
-          id: `L${('0000000' + (10 * page + index + 1)).slice(-7)}`,
-          book: {
-            id: 'b000001',
-            name: '图解HTTP',
-            author: '[日]上野 宣',
-          },
-          cardID: '1000001',
-          date: '2022-04-26',
-          duration: 2,
-        })),
-    });
+  async findAll(@Query('page') page = 1): Promise<ResponseData> {
+    return trueReturn(await this.lendService.findAll(page));
   }
 
   @Post()
-  create(
-    @Body() { cardID, bookID, duration }: CreateLendRecordDto,
-  ): ResponseData {
-    return trueReturn();
+  async create(
+    @Body() { bookID, cardID, duration }: CreateLendRecordDto,
+  ): Promise<ResponseData> {
+    const res = await this.lendService.create(bookID, cardID, duration);
+
+    return res ? trueReturn() : falseReturn();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): ResponseData {
+  async findOne(@Param('id') id: string): Promise<ResponseData> {
+    const {
+      id: ID,
+      book,
+      date,
+      duration,
+    } = await this.lendService.findOneByID(id);
+
     return trueReturn({
-      id,
-      record: '图解HTTP / [日]上野 宣 / 2022-04-26',
-      book: {
-        id: 'B000001',
-        name: '图解HTTP',
-        author: '[日]上野 宣',
-        isbn: '9798115351531',
-      },
-      date: '2022-04-26',
-      duration: 2,
+      ID,
+      record: `${book.name} / ${book.author} / ${date
+        .toISOString()
+        .slice(0, 10)}`,
+      book,
+      date,
+      duration: duration,
       isOverdue: true,
       fine: 30.26,
     });
   }
 
   @Get('card/:id')
-  findOneByCard(@Param('cardID') id: string): ResponseData {
+  async findOneByCard(@Param('id') id: string): Promise<ResponseData> {
+    const res = await this.lendService.findAllByCard(id);
+
     return trueReturn(
-      Array(2)
-        .fill('')
-        .map((_, index) => ({
-          id: String(index),
-          record: '图解HTTP / [日]上野 宣 / 2022-04-26',
-        })),
+      res.map(({ id: ID, book, date }) => ({
+        id: ID,
+        record: `${book.name} / ${book.author} / ${date
+          .toISOString()
+          .slice(0, 10)}`,
+      })),
     );
   }
 }

90041c1a7265682a9b29cf5d5eea36d57983d138
wangsz12 <1002590897@qq.com>
2022-05-11 16:32:01 +0800

    feature: 归还记录实体类新增date字段

--- backend/src/records/return/entity/return-record.entity.ts
+++ backend/src/records/return/entity/return-record.entity.ts
@@ -1,8 +1,8 @@
-import { IsBoolean, IsString, Length } from 'class-validator';
+import { IsBoolean, IsDate, IsString, Length } from 'class-validator';
 import { LendRecordEntity } from 'src/records/lend/entity/lend-record.entity';
 
 export class ReturnRecordEntity {
-  constructor(id: string, lend: LendRecordEntity, isOverdue: boolean) {
+  constructor(id: string, lend?: LendRecordEntity, isOverdue?: boolean) {
     this.id = id;
     this.lend = lend;
     this.isOverdue = isOverdue;
@@ -12,8 +12,11 @@ export class ReturnRecordEntity {
   @Length(8)
   id: string;
 
-  lend: LendRecordEntity;
+  lend?: LendRecordEntity;
+
+  @IsDate()
+  date: Date;
 
   @IsBoolean()
-  isOverdue: boolean;
+  isOverdue?: boolean;
 }

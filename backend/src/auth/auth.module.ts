59b333ff1e89df1237d30db36b1d7df5935b2986
wangsz12 <1002590897@qq.com>
2022-05-12 00:02:23 +0800

    feature: 引入jwt service并设置secret

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthDao } from './auth.dao';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [JwtModule.register({ secret: 'WanGSz12@DbWorK' })],
   controllers: [AuthController],
   providers: [AuthService, AuthDao],
 })

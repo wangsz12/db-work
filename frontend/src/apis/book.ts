764d053bfc4b9728ac473cc9c1c6ce8aad3253dd
wangsz12 <1002590897@qq.com>
2022-05-11 23:54:18 +0800

    feature: 图书部分对接真实接口

--- frontend/src/apis/book.ts
+++ frontend/src/apis/book.ts
@@ -1,5 +1,8 @@
+import Axios from "@/config/axios"
 import { mockAPIReturn } from "@/utils"
+import { PurchaseRequest } from "@/utils/types"
 
+// TODO 替换为真实接口
 export function getRecentRecord() {
   return mockAPIReturn(Array(10).fill('').map((_, index) => ({
     key: String(index),
@@ -13,44 +16,56 @@ export function getRecentRecord() {
 }
 
 export function getBookDataBox() {
-  return mockAPIReturn({
-    books: 1564,
-    lend: 745,
-    categories: 4
-  })
+  return Axios.get('/data-box/books')
+  // return mockAPIReturn({
+  //   books: 1564,
+  //   lend: 745,
+  //   categories: 4
+  // })
 }
 
 export function getBooksByPage(page = 1) {
-  console.log('page: ', page)
-  return mockAPIReturn({
-    total: 1564,
-    books: Array(10).fill('').map((_, index) => ({
-      id: `B${('0000000' + (10 * page + index + 1)).slice(-7)}`,
-      name: '图解HTTP',
-      author: '[日]上野 宣',
-      quantity: 10,
-      category: '计算机/网络',
-      publisher: '人民邮电出版社',
-      isbn: '9798115351531'
-    }))
+  return Axios.get('/books', {
+    params: {
+      page,
+    }
   })
+  // return mockAPIReturn({
+  //   total: 1564,
+  //   books: Array(10).fill('').map((_, index) => ({
+  //     id: `B${('0000000' + (10 * page + index + 1)).slice(-7)}`,
+  //     name: '图解HTTP',
+  //     author: '[日]上野 宣',
+  //     quantity: 10,
+  //     category: '计算机/网络',
+  //     publisher: '人民邮电出版社',
+  //     isbn: '9798115351531'
+  //   }))
+  // })
 }
 
 export function getBookByID(id: string) {
-  console.log('id: ', id)
-  return mockAPIReturn({
-    id,
-    name: '图解HTTP',
-    author: '[日]上野 宣',
-    isbn: '9798115351531',
-    price: 75.26
-  })
+  return Axios.get(`/books/${id}`)
+  // return mockAPIReturn({
+  //   id,
+  //   name: '图解HTTP',
+  //   author: '[日]上野 宣',
+  //   isbn: '9798115351531',
+  //   price: 75.26
+  // })
 }
 
 export function getBooksByPublisher(pid: string) {
-  console.log('pid: ', pid)
-  return mockAPIReturn(Array(3).fill('').map((_, index) => ({
-    id: `B${('0000000' + (index + 1)).slice(-7)}`,
-    record: '图解HTTP / [日]上野 宣'
-  })))
+  return Axios.get(`/books/publisher/${pid}`)
+  // return mockAPIReturn(Array(3).fill('').map((_, index) => ({
+  //   id: `B${('0000000' + (index + 1)).slice(-7)}`,
+  //   record: '图解HTTP / [日]上野 宣'
+  // })))
+}
+
+export function purchase({ bookID, quantity }: PurchaseRequest) {
+  return Axios.patch('/books/purchase', {
+    bookID,
+    quantity
+  })
 }

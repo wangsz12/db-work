8f09864b33de5a13df0b48a5e643140642885497
wangsz12 <1002590897@qq.com>
2022-05-11 23:54:34 +0800

    feature: 读者证部分对接真实接口

--- frontend/src/apis/card.ts
+++ frontend/src/apis/card.ts
@@ -1,36 +1,50 @@
-import { mockAPIReturn } from "@/utils"
+import Axios from "@/config/axios"
 import { CreateCardRequest } from "@/utils/types"
 
 export function getCardDataBox() {
-  return mockAPIReturn({
-    total: 1536,
-    male: 792,
-    female: 1536 - 792
-  })
+  return Axios.get('/data-box/cards')
+  // return mockAPIReturn({
+  //   total: 1536,
+  //   male: 792,
+  //   female: 1536 - 792
+  // })
 }
 
 export function getCardsByPage(page = 1) {
-  return mockAPIReturn({
-    total: 1536,
-    cards: Array(10).fill('').map((_, index) => ({
-      id: `1${('0000000' + (10 * page + index + 1)).slice(-7)}`,
-      name: '李华',
-      gender: '男',
-      contact: '13012345678',
-      address: 'XX市XX区XX路XX小区',
-      haveLent: 12
-    }))
+  return Axios.get('/cards', {
+    params: {
+      page
+    }
   })
+  // return mockAPIReturn({
+  //   total: 1536,
+  //   cards: Array(10).fill('').map((_, index) => ({
+  //     id: `1${('0000000' + (10 * page + index + 1)).slice(-7)}`,
+  //     name: '李华',
+  //     gender: '男',
+  //     contact: '13012345678',
+  //     address: 'XX市XX区XX路XX小区',
+  //     haveLent: 12
+  //   }))
+  // })
 }
 
 export function deleteCardByID(id: string){
-  console.log('id: ', id)
-  return mockAPIReturn()
+  return Axios.delete('/cards', {
+    params: {
+      id
+    }
+  })
 }
 
 export function createCard({ name, gender, contact, address }: CreateCardRequest) {
-  console.log('name, gender, contact, address: ', name, gender, contact, address)
-  return mockAPIReturn({
-    id: 'R000001'
+  return Axios.post('/cards', {
+    name,
+    gender,
+    contact,
+    address
   })
+  // return mockAPIReturn({
+  //   id: 'R000001'
+  // })
 }

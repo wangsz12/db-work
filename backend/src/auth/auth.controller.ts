3f5757adb005a7492f285b5901fc7c1e40f50ed2
wangsz12 <1002590897@qq.com>
2022-05-12 00:02:11 +0800

    feature: 新增生成token功能

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { ResponseData } from 'src/types';
 import { falseReturn, trueReturn } from 'src/utils';
 import { AuthService } from './auth.service';
@@ -6,12 +7,23 @@ import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Post('login')
   @HttpCode(200)
   async login(@Body() { account, password }: LoginDto): Promise<ResponseData> {
     const res = await this.authService.login(account, password);
-    return res ? trueReturn(res) : falseReturn(null, '用户名或密码错误');
+    if (typeof res !== 'boolean') {
+      const token: string = this.jwtService.sign({ id: res.id });
+      return trueReturn({
+        ...res,
+        token,
+      });
+    } else {
+      return falseReturn(null, '用户名或密码错误');
+    }
   }
 }

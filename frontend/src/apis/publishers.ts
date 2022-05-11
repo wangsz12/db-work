d5c771472ff27b7b79ab121599a724d347e869ac
wangsz12 <1002590897@qq.com>
2022-05-11 23:55:58 +0800

    feature: 出版社部分对接真实接口

--- frontend/src/apis/publishers.ts
+++ frontend/src/apis/publishers.ts
@@ -1,32 +1,32 @@
-import { mockAPIReturn } from "@/utils"
-import { PurchaseRequest } from "@/utils/types"
+import Axios from "@/config/axios"
 
 export function getPublisherDataBox() {
-  return mockAPIReturn({
-    publishers: 52
-  })
+  return Axios.get('/data-box/publishers')
+  // return mockAPIReturn({
+  //   publishers: 52
+  // })
 }
 
 export function getPublisherByPage(page = 1) {
-  return mockAPIReturn({
-    total: 52,
-    publishers: Array(10).fill('').map((_, index) => ({
-      id: `P${('0000000' + (10 * page + index + 1)).slice(-7)}`,
-      name: '人民邮电出版社',
-      contact: '13012345678',
-      address: 'XX市XX区XX路1号'
-    }))
+  return Axios.get('/publishers', {
+    params: {
+      page
+    }
   })
+  // return mockAPIReturn({
+  //   total: 52,
+  //   publishers: Array(10).fill('').map((_, index) => ({
+  //     id: `P${('0000000' + (10 * page + index + 1)).slice(-7)}`,
+  //     name: '人民邮电出版社',
+  //     contact: '13012345678',
+  //     address: 'XX市XX区XX路1号'
+  //   }))
+  // })
 }
 
 export function getPublisherByID(id: string) {
-  console.log('id: ', id)
-  return mockAPIReturn({
-    name: '人民邮电出版社'
-  })
-}
-
-export function purchase({ bookID, price }: PurchaseRequest) {
-  console.log('bookID, price: ', bookID, price)
-  return mockAPIReturn()
+  return Axios.get(`/publishers/${id}`)
+  // return mockAPIReturn({
+  //   name: '人民邮电出版社'
+  // })
 }

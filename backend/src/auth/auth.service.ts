d3e9363b903622e19d47d8a9f14fb968bdf6180a
wangsz12 <1002590897@qq.com>
2022-05-15 18:43:24 +0800

    feature: 密码做两次md5加密

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { AuthDao } from './auth.dao';
-import { AdminEntity } from './entity/admin.entity';
+import { AdminEntity } from '../admin/entity/admin.entity';
+import md5 = require('blueimp-md5');
 
 @Injectable()
 export class AuthService {
@@ -10,6 +11,7 @@ export class AuthService {
     account: string,
     password: string,
   ): Promise<AdminEntity | boolean> {
+    password = md5(md5(password));
     return await this.authDao.login(account, password);
   }
 }

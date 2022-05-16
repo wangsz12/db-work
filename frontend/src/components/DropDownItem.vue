bc5b59a55f36291df8317bac0f5a3fe303d4b415
wangsz12 <1002590897@qq.com>
2022-05-16 10:04:06 +0800

    feature: 主布局右上角下拉菜单条目组件

---
+++ frontend/src/components/DropDownItem.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+interface Props {
+  text: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  text: '',
+})
+const emits = defineEmits(['click'])
+</script>
+
+<template>
+  <div
+    class="dropdown-item"
+    @click="emits('click')"
+  >
+    <div class="dropdown-content">
+      <slot />
+      <span>{{ props.text }}</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .dropdown-item {
+    height: 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: background-color linear .15s;
+    padding: 3px 0;
+
+    &:hover {
+      background-color: rgb(242, 240, 254);
+      transition: background-color linear .15s;
+      cursor: pointer;
+      color: $theme-color;
+    }
+
+    .dropdown-content {
+      font-size: .9rem;
+      display: flex;
+      align-items: center;
+
+      span:nth-child(2) {
+        padding-left: 10px;
+      }
+    }
+  }
+</style>

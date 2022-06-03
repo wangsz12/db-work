d173914452c3d58f88baf96f0b1fe4f807d4b086
wangsz12 <1002590897@qq.com>
2022-06-03 19:40:39 +0800

    feature: 将接口替换为真实接口 feature: 新增获取全部子分类接口

--- frontend/src/apis/category.ts
+++ frontend/src/apis/category.ts
@@ -1,35 +1,38 @@
-import { mockAPIReturn } from "@/utils"
+import Axios from "@/config/axios"
 import { CreateCategoryRequest } from "@/utils/types"
 
 export function getCategoryDataBox() {
-  return mockAPIReturn({
-    total: 253,
-    parents: 22
-  })
+  return Axios.get('/data-box/categories')
 }
 
 export function getCategoriesByPage(page = 1) {
-  return mockAPIReturn({
-    total: 253,
-    categories: Array(10).fill('').map((_, index) => ({
-      id: String(index),
-      name: 'xxx',
-      parent: 'XXX'
-    }))
+  return Axios.get('/categories', {
+    params: {
+      page
+    }
   })
 }
 
 export function deleteCategoryByID(id: string) {
-  return mockAPIReturn()
+  return Axios.delete('/categories', {
+    params: {
+      id
+    }
+  })
 }
 
 export function getAllParentCategories() {
-  return mockAPIReturn(Array(22).fill('').map((_, index) => ({
-    id: String(index),
-    name: String(index)
-  })))
+  return Axios.get('/categories/parent')
 }
 
-export function createCategory({ name, parent }: CreateCategoryRequest) {
-  return mockAPIReturn()
+export function getAllSubCategories() {
+  return Axios.get('/categories/sub')
+}
+
+export function createCategory({ id, name, parent }: CreateCategoryRequest, isParent: boolean) {
+  return Axios.post('/categories', {
+    id,
+    name,
+    parent: isParent ? null : parent
+  })
 }

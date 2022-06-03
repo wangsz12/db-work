8a5781ae2981780570c53b7ca5982a73930a899f
wangsz12 <1002590897@qq.com>
2022-06-03 17:09:23 +0800

    feature: 完善api返回结果中data的类型，新增数组类型

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -1,6 +1,6 @@
 interface APIResult {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  readonly data: any,
+  readonly data: any | any[],
   readonly msg: string,
   readonly status: boolean
 }
@@ -87,4 +87,14 @@ export interface CreateAdminRequest {
   name: string;
   account: string;
   password: string;
+}
+
+export interface CreateCategoryRequest {
+  name: string;
+  parent: string;
+}
+
+export interface ParentCategory {
+  id: string;
+  name: string;
 }

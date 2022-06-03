7096598788e73c91ea2ae73a5f9e3d70d395981e
wangsz12 <1002590897@qq.com>
2022-06-03 19:42:10 +0800

    feature: 新增添加父分类功能

--- frontend/src/pages/categories/new-category.vue
+++ frontend/src/pages/categories/new-category.vue
@@ -1,31 +1,38 @@
 <script setup lang="ts">
 import { createCategory, getAllParentCategories } from '@/apis/category'
 import { useMessage } from '@/utils'
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { ParentCategory } from '@/utils/types'
+import { Category } from '@/utils/types'
 
 const $message = useMessage()
 const router = useRouter()
 
 const form = reactive({
+  id: '',
   name: '',
   parent: ''
 })
-const parents: ParentCategory[] = reactive([])
+const parents: Category[] = reactive([])
+const isParent = ref(false)
 
 getAllParentCategories()
   .then(({data: res}) => {
     parents.push(...res.data)
   })
 
+function handleParentCheck(value: boolean | (string | number | boolean)[]) {
+  isParent.value = value as boolean
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function handleSubmit({values, errors}: {values: any, errors: unknown}) {
   if (errors) {
     return
   }
+    console.log('values: ', values)
 
-  createCategory(values)
+  createCategory(values, isParent.value)
     .then(() => {
       $message.success('新增分类成功')
       router.push('/categories')
@@ -49,6 +56,29 @@ function handleSubmit({values, errors}: {values: any, errors: unknown}) {
           :size="12"
         >
           <a-form-item
+            field="id"
+            label="编号"
+            :rules="[
+              {
+                required: true,
+                message: '编号为必填'
+              },
+              {
+                minLength: 1,
+                message: '编号长度最短为1'
+              },
+              {
+                maxLength: 3,
+                message: '编号长度最长为3'
+              }
+            ]"
+          >
+            <a-input
+              v-model="form.id"
+              placeholder="请输入编号"
+            />
+          </a-form-item>
+          <a-form-item
             field="name"
             label="名称"
             :rules="[
@@ -63,23 +93,30 @@ function handleSubmit({values, errors}: {values: any, errors: unknown}) {
               placeholder="请输入名称"
             />
           </a-form-item>
+          <a-form-item>
+            <a-checkbox @change="handleParentCheck">
+              该分类是父分类
+            </a-checkbox>
+          </a-form-item>
           <a-form-item
             field="parent"
             label="父分类"
-            :rules="[
-              {
+            :rules="isParent
+              ? []
+              : [{
                 required: true,
-                message: '父分类为必选'
-              }
-            ]"
+                message: '名称为必填'
+              }]"
           >
             <a-select
               v-model="form.parent"
               placeholder="请选择父分类"
+              :disabled="isParent"
             >
               <a-option
                 v-for="item in parents"
                 :key="item.id"
+                :value="item.id"
               >
                 {{ item.name }}
               </a-option>

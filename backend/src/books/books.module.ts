e3fee7d95fbcfcc97ed81320a66b982977157fe7
wangsz12 <1002590897@qq.com>
2022-06-03 19:47:02 +0800

    feature: 引入分类模块的Dao

--- backend/src/books/books.module.ts
+++ backend/src/books/books.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CategoriesDao } from 'src/categories/categories.dao';
 import { BooksController } from './books.controller';
 import { BooksDao } from './books.dao';
 import { BooksService } from './books.service';
 
 @Module({
   controllers: [BooksController],
-  providers: [BooksService, BooksDao],
+  providers: [BooksService, BooksDao, CategoriesDao],
 })
 export class BooksModule {}
